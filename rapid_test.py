0a72c1e5d1ad17402a318d38156d9aaea2fa4b17
Matthew Lowery <mattlowery@Matthews-MBP.nyc.rr.com>
2021-02-18 15:06:09 -0500

    covid sux

--- rapid_test.py
+++ rapid_test.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
-def scrip(): 
+from config import keys 
 
-	browser = webdriver.Firefox()
+def scrip(k, browser): 
 
 	browser.get("https://v2.waitwhile.com/welcome/mmdgraham")
 
@@ -16,37 +16,25 @@ def scrip():
 	time.sleep(2)
 	browser.find_element_by_xpath('/html/body/app-root/public-landing-page/main/div/div/public-resource/div/div/div/div/div[2]/button[1]/div').click()
 	time.sleep(2)
-	browser.find_element_by_xpath('//*[@id="name02"]').send_keys("Anna")
+	browser.find_element_by_xpath('//*[@id="name02"]').send_keys(k['f_name'])
 	time.sleep(2)
-	browser.find_element_by_xpath('//*[@id="name03"]').send_keys("Molloy")
+	browser.find_element_by_xpath('//*[@id="name03"]').send_keys(k['l_name'])
 	time.sleep(2)
-	browser.find_element_by_xpath('//*[@id="phone01"]').send_keys("9173738847")
+	browser.find_element_by_xpath('//*[@id="phone01"]').send_keys(k['tel'])
 	time.sleep(2)
-	browser.find_element_by_xpath('//*[@id="AUwcZ8ZlfWBDC9pxyZSy"]').send_keys("19")
+	browser.find_element_by_xpath('//*[@id="AUwcZ8ZlfWBDC9pxyZSy"]').send_keys(k['age'])
 	time.sleep(2)
-	browser.find_element_by_xpath('/html/body/app-root/public-landing-page/main/div/div/public-confirm-page/div/div/div/div[2]/form/div[1]/dynamic-form/form-multi-select/div/div[1]/ww-multiselect/ng-select/div/div/div[2]/input').send_keys('blue')
+	browser.find_element_by_xpath('/html/body/app-root/public-landing-page/main/div/div/public-confirm-page/div/div/div/div[2]/form/div[1]/dynamic-form/form-multi-select/div/div[1]/ww-multiselect/ng-select/div/div/div[2]/input').send_keys(k['insurance_keyword'])
 	time.sleep(2)
 	browser.find_element_by_xpath('/html/body/app-root/public-landing-page/main/div/div/public-confirm-page/div/div/div/div[2]/form/div[1]/dynamic-form/form-multi-select/div/div[1]/ww-multiselect/ng-select/ng-dropdown-panel/div[2]').click()
 	time.sleep(2)
-#browser.find_element_by_xpath('//*[@id="public-waitlist-confirm"]/div/div/div[2]/form/div[1]/button').click()
+	browser.find_element_by_xpath('/html/body/app-root/public-landing-page/main/div/div/public-confirm-page/div/div/div/div[2]/form/div[1]/button').click()
 
-scrip()
+if __name__ == "__main__":
+	browser = webdriver.Firefox()
+	scrip(keys, browser)
 
-# browser.quit()class="ng-dropdown-panel-items scroll-host"
 
-# #browser.find_element_by_xpath("//*[@id="a9ae4aa4ff37-2"]")
-
-# keys = {
-#         # need the url before hand
-#         "start_url": "https://v2.waitwhile.com/welcome/mmdgraham",
-#         # must be capital letters
-#         "name": "Matthew Lowery",
-#         "tel": 4439870848,
-#         # address can be 'jigged' to use for multiple orders; i.e. use rd instead of road st instead of st
-#         "address": "1 heaven way",
-#         # or adding/withdrawing an apt number when you live in a house
-
-# }
 
 
 
